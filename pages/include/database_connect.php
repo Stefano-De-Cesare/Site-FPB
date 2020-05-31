0ec17f6a65659fc22795ef9c48636831c219ee1f
Stefano-De-Cesare <65348662+Stefano-De-Cesare@users.noreply.github.com>
2020-05-31 13:34:31 -0300

    v0.0.3
    tentando fazer as pessoas verem o site

--- pages/include/database_connect.php
+++ pages/include/database_connect.php
@@ -4,12 +4,19 @@ $username = "id11952728_shiroserveradmin";
 $password = "t9vmr&7h)w=Gw>\b";
 $database = "id11952728_dbsugestoes";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-    } catch(PDOException $e) {    
-    echo "Connection failed: " . $e->getMessage();
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if(!$conn){
+    die("Connection failed: ".mysqli_connect_error());
+}
+    
+function setSuggetions($conn){
+    if(isset($_POST['submitSuggestion'])){
+        $nomeAutor = $_POST['nome_autor'];
+        $date = $_POST['date'];
+        $suggestionText = $_POST['suggestion'];
+        
+        $sqlCode = "INSERT INTO sugestoes (nome_autor, date, message) VALUES ('$nomeAutor', '$date', '$suggestionPost')"
+        $result = $conn->query($sqlCode);
     }
-?>
+}
--- pages/sugestoes.php
+++ pages/sugestoes.php
@@ -1,5 +1,6 @@
 <?php include("include/navbar.html");
-include("include/database_connect.php");?>
+include("include/database_connect.php");
+date_default_timezone_set('America/Brasilia');?>
 <!DOCTYPE html>
 <html lang="pt">
 
@@ -11,12 +12,48 @@ include("include/database_connect.php");?>
 
 
 <style>
+    input{
+        width: 500px;
+        height: 30px;
+        background-color= #fff;
+        font-family: arial;
+        font-weight: 400;
+        border: none;
+    }
+    textarea{
+        width: 500px;
+        height: 240px;
+        background-color= #fff;
+        resize: none;
+        font-family: arial;
+        font-weight: 400;
+        border: none;
+}
+    button{
+        width: 100px;
+        height: 30px;
+        background-color: #282828;
+        border: none;
+        color: #fff;
+        font-family: arial;
+        font-weight: 400;
+        cursor: pointer;
+    }
 </style>
 
 <body>
 	<img src="../assets/1.png">
 	
 	<div class="jumbotron">
+	
+<?php
+	echo "<form method='POST' action='".setSuggetions($conn)."'>
+	     <input type='text' name='nome_autor' maxlength='128'><br><br>
+	     <input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'>
+	     <textarea name='suggestion'></textarea><br>
+	     <button type='submit' name='submitSuggestion'>Enviar</button>
+	</form>";
+?>	
 	</div>
 </body>
 </html>
